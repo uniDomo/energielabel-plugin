be3ff2c82209dbcf42fce97cbfe85c55e958e7e4
Jan H. Hansen <clonck.jh@googlemail.com>
2018-07-19 12:06:28 +0200

    change of heart

--- src/Controllers/EnergielabelController.php
+++ src/Controllers/EnergielabelController.php
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace Energielabel\Controllers;
-
-use Plenty\Plugin\Controller;
-use Plenty\Plugin\Templates\Twig;
-
-class EnergielabelController extends Controller
-{
-    /**
-     * @param Twig $twig
-     * @return string
-     */
-    public function getHelloWorldPage(Twig $twig):string
-    {
-        return $twig->render('Energielabel::Energielabel');
-    }
-=======
 <?php
 
 namespace Energielabel\Controllers;
@@ -34,5 +15,4 @@ class EnergielabelController extends Controller
     {
         return $twig->render('Energielabel::Index');
     }
->>>>>>> abe9a0e2ef12b5a83cc477f9aa09f11cc73431f8
 }
