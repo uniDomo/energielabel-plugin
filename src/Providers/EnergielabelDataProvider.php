2072b4f202dc50a451491b5d8e4ec7302e68b7fc
Jan Hendrik Hansen <hansen@unidomo.com>
2018-07-19 11:54:44 +0200

    ajj

--- src/Providers/EnergielabelDataProvider.php
+++ src/Providers/EnergielabelDataProvider.php
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-﻿<?php
-
-namespace Energielabel\Providers;
+﻿﻿<?php
 
 use Plenty\Plugin\Templates\Twig;
 
@@ -11,18 +8,4 @@ class EnergielabelDataProvider
     {
         return $twig->render('Energielabel:Energielabel');
     }
-=======
-﻿<?php
-
-namespace Energielabel\Providers;
-
-use Plenty\Plugin\Templates\Twig;
-
-class EnergielabelDataProvider
-{
-    public function call ( Twig $twig )
-    {
-        return $twig->render('Energielabel:Energielabel');
-    }
->>>>>>> abe9a0e2ef12b5a83cc477f9aa09f11cc73431f8
 }
