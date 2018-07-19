abe9a0e2ef12b5a83cc477f9aa09f11cc73431f8
Jan H. Hansen <clonck.jh@googlemail.com>
2018-07-19 11:24:20 +0200

    added basic configuration

---
+++ src/Providers/EnergielabelDataProvider.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+namespace Energielabel\Providers;
+
+use Plenty\Plugin\Templates\Twig;
+
+class EnergielabelDataProvider
+{
+    public function call ( Twig $twig )
+    {
+        return $twig->render('Energielabel:Energielabel');
+    }
+}
