1125a7e4c7dbeaece3068a87b315a33e7dce73e4
Jan H. Hansen <clonck.jh@googlemail.com>
2018-07-19 12:23:55 +0200

    und ersetzen

--- src/Controllers/EnergielabelController.php
+++ src/Controllers/EnergielabelController.php
@@ -1,3 +1,22 @@
+<<<<<<< HEAD
+<?php
+
+namespace Energielabel\Controllers;
+
+use Plenty\Plugin\Controller;
+use Plenty\Plugin\Templates\Twig;
+
+class EnergielabelController extends Controller
+{
+    /**
+     * @param Twig $twig
+     * @return string
+     */
+    public function getHelloWorldPage(Twig $twig):string
+    {
+        return $twig->render('Energielabel::Energielabel');
+    }
+=======
 <?php
 
 namespace Energielabel\Controllers;
@@ -15,4 +34,5 @@ class EnergielabelController extends Controller
     {
         return $twig->render('Energielabel::Index');
     }
+>>>>>>> abe9a0e2ef12b5a83cc477f9aa09f11cc73431f8
 }
